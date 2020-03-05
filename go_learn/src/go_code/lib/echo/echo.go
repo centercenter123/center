47dce8bd32b1f21a648d40353fbee06804f0df33
xzhong <xzhong@i9i8.com>
2020-03-05 09:37:14 +0800

    #实例代码

---
+++ go_learn/src/go_code/lib/echo/echo.go
@@ -0,0 +1,8 @@
+package echo
+import (
+	"fmt"
+)
+
+func Echo(str string) {
+	fmt.Println(str)
+}
---
+++ go_learn/src/go_code/main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"go_code/lib/echo"
+)
+
+func main() {
+	echo.Echo("123")
+}
